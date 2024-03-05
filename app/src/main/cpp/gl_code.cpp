90b1f96b351cb86ec1129c59579b8d7acb22be94
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-03-04 23:26:16 -0800

    missing files

--- app/src/main/cpp/gl_code.cpp
+++ app/src/main/cpp/gl_code.cpp
@@ -165,21 +165,21 @@ void renderFrame() {
 
 extern "C" {
 JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv* env,
-                                                              jobject obj,
-                                                              jint width,
-                                                              jint height);
+                                      jobject obj,
+                                      jint width,
+                                      jint height);
 JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_step(JNIEnv* env,
-                                                              jobject obj);
+                                      jobject obj);
 };
 
 JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv* env,
-                                                              jobject obj,
-                                                              jint width,
-                                                              jint height) {
+                                      jobject obj,
+                                      jint width,
+                                      jint height) {
   setupGraphics(width, height);
 }
 
 JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_step(JNIEnv* env,
-                                                              jobject obj) {
+                                      jobject obj) {
   renderFrame();
 }
